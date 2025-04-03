1e2462e46e7ec8d6a56347a1c1d3f240512bd1d5
KevinB6766 <tuu21653@temple.edu>
2025-04-03 12:19:59 -0400

    Implemented service connection and timer functionality.

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,21 +1,47 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    private var timerBinder: TimerService.TimerBinder? = null
+    val timeHandler = Handler(Looper.getMainLooper()){
+        findViewById<TextView>(R.id.textView).text = it.what.toString()
+        true
+    }
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
-
+    val serviceConnection = object : ServiceConnection { //service connection
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) { //service connected
+            timerBinder = service as TimerService.TimerBinder
+            timerBinder?.setHandler(timeHandler) //sets handler
         }
-        
-        findViewById<Button>(R.id.stopButton).setOnClickListener {
 
+        override fun onServiceDisconnected(name: ComponentName?) { //service disconnected
+            timerBinder = null
         }
     }
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+        bindService(Intent(this, TimerService::class.java), serviceConnection, BIND_AUTO_CREATE) //binds service
+
+            findViewById<Button>(R.id.startButton).setOnClickListener {//start button
+                if (timerBinder?.isRunning == false) //if timer is not running
+                    timerBinder?.start(10) //starts timer
+            }
+            findViewById<Button>(R.id.stopButton).setOnClickListener { //stop button
+                timerBinder?.stop() //stops timer
+            }
+        }
 }
