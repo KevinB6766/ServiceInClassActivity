6779453cd4a9b5eaf2e573a2d216b9aee5cffb90
KevinB6766 <tuu21653@temple.edu>
2025-04-10 11:42:32 -0400

    Modified implementation: added menu and removed buttons in main activity layout.

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -9,18 +9,23 @@ import android.content.ServiceConnection
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     private var timerBinder: TimerService.TimerBinder? = null
-    val timeHandler = Handler(Looper.getMainLooper()){
+    val timeHandler = Handler(Looper.getMainLooper()) {
         findViewById<TextView>(R.id.textView).text = it.what.toString()
         true
     }
 
     val serviceConnection = object : ServiceConnection { //service connection
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) { //service connected
+        override fun onServiceConnected(
+            name: ComponentName?,
+            service: IBinder?
+        ) { //service connected
             timerBinder = service as TimerService.TimerBinder
             timerBinder?.setHandler(timeHandler) //sets handler
         }
@@ -30,18 +35,45 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
-        bindService(Intent(this, TimerService::class.java), serviceConnection, BIND_AUTO_CREATE) //binds service
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-            findViewById<Button>(R.id.startButton).setOnClickListener {//start button
-                if (timerBinder?.isRunning == false) //if timer is not running
-                    timerBinder?.start(10) //starts timer
+        bindService(
+            Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        ) //binds service
+    }
+
+    fun stopButton() {
+        timerBinder?.stop()
+    }//stops timer
+
+    fun startButton() {
+        if (timerBinder?.isRunning == false) //if timer is not running
+            timerBinder?.start(10) //starts timer
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_start -> {
+                startButton()
+                return true
             }
-            findViewById<Button>(R.id.stopButton).setOnClickListener { //stop button
-                timerBinder?.stop() //stops timer
+
+            R.id.action_stop -> {
+                stopButton()
+                return true
             }
+            else -> return false
         }
+        return true //tells the system that the item was selected /
+                    // and that the item was handled here
+    }
 }
