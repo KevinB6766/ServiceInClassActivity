140c6befa8a5f72dbbb82c2f38d084a72ca6a6c4
KevinB6766 <tuu21653@temple.edu>
2025-04-16 15:34:45 -0400

    Modified implementation to include shared preferences for saving countdown value.

--- app/src/main/java/edu/temple/myapplication/TimerService.kt
+++ app/src/main/java/edu/temple/myapplication/TimerService.kt
@@ -2,6 +2,7 @@ package edu.temple.myapplication
 
 import android.app.Service
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Binder
 import android.os.Handler
 import android.os.IBinder
@@ -17,6 +18,15 @@ class TimerService : Service() {
     lateinit var t: TimerThread
 
     private var paused = false
+
+    private lateinit var sharedPreferences: SharedPreferences //shared preferences
+    private var savedValue: Int = 0 //saved value
+
+    companion object {
+        const val PREFS_NAME = "TimerPrefs" //shared preferences
+        const val COUNTDOWN_KEY = "countdown_value" //value
+        const val DEFAULT_VALUE = 100 //default value
+    }
 
     inner class TimerBinder : Binder() {
 
@@ -62,6 +72,9 @@ class TimerService : Service() {
 
     override fun onCreate() {
         super.onCreate()
+        Log.d("TimerService status", "Created")
+        sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE) //shared preferences
+        savedValue = sharedPreferences.getInt(COUNTDOWN_KEY, DEFAULT_VALUE) //saved value
 
         Log.d("TimerService status", "Created")
     }
@@ -79,15 +92,21 @@ class TimerService : Service() {
         if (::t.isInitialized) {
             paused = !paused
             isRunning = !paused
+            if (paused) {
+                saveCountdownValue(t.currentValue) //saves countdown value
+            }
         }
     }
 
     inner class TimerThread(private val startValue: Int) : Thread() {
 
+        var currentValue: Int = startValue //current value
+
         override fun run() {
             isRunning = true
             try {
                 for (i in startValue downTo 1)  {
+                    currentValue = i //update current value
                     Log.d("Countdown", i.toString())
 
                     timerHandler?.sendEmptyMessage(i)
@@ -97,10 +116,12 @@ class TimerService : Service() {
 
                 }
                 isRunning = false
+                saveCountdownValue(DEFAULT_VALUE)
             } catch (e: InterruptedException) {
                 Log.d("Timer interrupted", e.toString())
                 isRunning = false
                 paused = false
+                saveCountdownValue(DEFAULT_VALUE)
             }
         }
 
@@ -118,6 +139,12 @@ class TimerService : Service() {
         super.onDestroy()
 
         Log.d("TimerService status", "Destroyed")
+    }
+    private fun saveCountdownValue(value: Int) { //saves countdown value
+        with(sharedPreferences.edit()) { //shared preferences
+            putInt(COUNTDOWN_KEY, value)
+            apply() //saves value
+        }
     }
 
 
